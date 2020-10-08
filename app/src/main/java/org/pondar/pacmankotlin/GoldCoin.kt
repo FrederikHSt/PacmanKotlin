938d9325d930b8d5ad9bfdebe7cc81a62e82e606
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2020-10-08 21:16:54 +0200

    Update GoldCoin.kt

--- app/src/main/java/org/pondar/pacmankotlin/GoldCoin.kt
+++ app/src/main/java/org/pondar/pacmankotlin/GoldCoin.kt
@@ -1,4 +1,19 @@
 package org.pondar.pacmankotlin
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+
 //Here you need to fill out what should be in a GoldCoin and what should the constructor be
-class GoldCoin() {}
+class GoldCoin(arrayX: Int, arrayY: Int) {
+    var x = 0
+    var y = 0
+
+    var taken = false
+
+    init {
+        x = arrayX
+        y = arrayY
+    }
+
+
+}
